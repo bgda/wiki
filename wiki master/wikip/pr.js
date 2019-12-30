0ba705b3277862fe28e33be0b59960fb77aa4155
bgda <unitymaxx1@gmail.com>
2019-12-30 10:56:45 -0300

    Trying this

---
+++ wiki master/wikip/pr.js
@@ -0,0 +1,22 @@
+//window.onload = document.getElementById("fi").focus();
+
+function boton() {
+    field = fi.value;
+    if (field== "1") {
+        r = document.getElementById("rem");
+        document.getElementById("body").removeChild(r);
+        var imagen = document.createElement("img");
+        imagen.src= "ejemplo.jpg";
+        document.getElementById("body").appendChild(imagen);
+    }
+}
+
+var tg = document.getElementsByTagName("input")[0];
+window.onload = function ij(){tg.focus();}
+
+
+var chus = document.getElementById("di");
+
+/*
+<img src="ejemplo.jpg" id="efe" alt="">
+    <script src="pr.js"></script>*/
