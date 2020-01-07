10fbc690f65d66d5e4566ec2033e3c34095a03b9
bgda <unitymaxx1@gmail.com>
2020-01-07 13:06:15 -0300

    z

--- wiki master/wikip/pr.js
+++ wiki master/wikip/pr.js
@@ -1,12 +1,26 @@
 
 function boton() {
     field = fi.value;
-    if (field== "1") {
+    if (field== "hermoso cabello") {
         r = document.getElementById("rem");
         document.getElementById("body").removeChild(r);
         var imagen = document.createElement("img");
-        imagen.src= "wiki master/wikip/ejemplo.jpg";
+        imagen.src= "wiki master/wikip/cft.jpg";
         document.getElementById("body").appendChild(imagen);
+
+        var niv = document.createElement("video");
+        niv.src= "wiki master/wikip/piu.mp4";
+        niv.videoHeight="1280"; niv.videoWidth="720";
+        niv.controls= true;
+        niv.setAttribute("class", "borde");
+        niv.setAttribute("id", "bid");
+        document.getElementById("body").appendChild(niv);
+         corona = document.createElement("p");
+         ic = document.createTextNode("👑");
+        corona.appendChild(ic);
+        document.getElementById("bid").appendChild(corona);
+        body.appendChild(corona);
+        
     }
 }
 /*
